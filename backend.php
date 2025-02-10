9aff1d15be4c232d77b64ea1887f419fa52afb41
Dulneth210229 <dulneth.sa@gmail.com>
2025-02-10 14:06:06 +0530

    Implemented middleware to handle client request

--- backend.php
+++ backend.php
@@ -1,125 +1,28 @@
 <?php
-    require_once 'config.php';
+    require_once 'student.php';
 
-    class Student{
-        
-        private $db;
+    $student = new Student();
 
-        public function __construct(){
-            $database = new Database();
-            $this->db = $database->getConnection();
-        }
+    if(isset($_GET['action'])){
 
-        //Function for add a new students
-        public function addStudent($name, $birth, $gender, $resume){
-
-            $birthDate = new DateTime($birth);
-            $today = new DateTime();
-            $age = $today->diff($birthDate)->y;
-
-            $uploadDir = 'uploads/';
-            if(!is_dir($uploadDir)){
-                mkdir($uploadDir, 0777, true);
-            }
-
-            $fileName = uniqid() . '_' . basename($resume['name']);
-            $filePath = $uploadDir . $fileName;
-
-            if(move_uploaded_file($resume['tmp_name'], $filePath)){
-                $sql = "INSERT INTO student (nam, birth, age, gender, resume) VALUES (?, ?, ?, ?, ?)";
-                $stmt = $this->db->prepare($sql);
-                $stmt->bind_param("ssiss", $name, $birth, $age, $gender, $filePath);
-
-                if($stmt->execute()){
-                    return "Student added Successfully";
-                }else{
-                    return "Error Adding Student" . $stmt->error;
-                }
-            }else{
-                return "Error Uploading resume";
-            }
-        }
-
-        //Function for Fetch all students
-        public function fetchStudents(){
-
-            $sql = "SELECT * FROM student";
-            $result = $this->db->query($sql);
-
-            if($result){
-                $student = $result->fetch_all(MYSQLI_ASSOC);
-                return json_encode($student);
-            }else{
-                return json_encode(["Error" => "Error Fetching Students"]);
-            }
-
-
-        }
-
-         //Function for fetch a single student by ID
-        public function fetchStudentById($id){
-            $sql = "SELECT * FROM students WHERE id = ?";
-            $stmt = $this->db->prepare($sql);
-            $stmt->bind_param("i", $id);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            if($result->num_rows > 0){
-                return json_encode($result->fetch_assoc());
-            }else{
-                return json_encode(["error" => "Student not found"]);
-            }
-        }
-
-        //Function for update student
-        public function updateStudent($id, $name, $birth, $gender, $resume){
-
-            $birthDate = new Datetime($birth);
-            $today = new DateTime();
-            $age = $today->diff($birthDate)->y;
-
-            $filePath = null;
-            if($resume['error' === UPLOAD_ERR_OK]){
-                $uploadDir = 'uploads/';
-                if(!is_dir($uploadDir)){
-                    mkdir($uploadDir, 0777, true); //making the directory if not exist and give read and write permission
-                }
-
-                $fileName = uniqid() . '_' . basename($resume['name']);
-                $filePath = $uploadDir . $fileName;
-
-                if(!move_uploaded_file($resume['tmp_name'], $filePath)){
-                    return "Error Uploading Resume";
-                }
-            }
-            if($filePath){
-                $sql = "UPDATE student SET name = ?, birth = ?, age =?, gender = ?, resume = ? WHERE id = ?";
-                $stmt = $this->db->prepare($sql);
-                $stmt->bind_param("ssissi", $name, $birth, $age, $gender, $filePath, $id);
-            }else{
-                $sql = "UPDATE student SET name = ?, birth = ?, age = ?, gender = ? WHERE id = ?";
-                $stmt = $this->db->prepare($sql);
-                $stmt->bind_param("ssisi", $name, $birth, $age, $gender, $id);   
-            }
-            if ($stmt->execute()){
-                return "Student Updated Successfully";
-            }else{
-                return "Error updating student" . $stmt->error;
-            }
-
-        }
-
-        //Function for delete student
-        public function deleteStudent($id){
-            $sql = "DELETE FROM student WHERE id = ?";
-            $stmt = $this->db->prepare($sql);
-            $stmt->bind_param("i", $id);
-
-            if($stmt->execute()){
-                return "Student deleted successfully";
-            }else{
-                return "Error deleting student : " . $stmt->error;
-            }
+        switch ($_GET['action']){
+            case 'add':
+                echo $student->addStudent($_POST['name'], $_POST['birth'], $_POST['gender'], $_FILES['resume']);
+                break;
+            case 'fetch':
+                echo $student->fetchStudents();
+                break;
+            case 'fetchById':
+                echo $student->fetchStudentById($_POST['id']);
+                break;
+            case 'update': 
+                echo $student->updateStudent($_POST['id'], $_POST['name'], $_POST['birth'], $_POST['gender'], $_FILES['resume']);
+                break;
+            case 'delete':
+                echo $student->deleteStudent($_POST['id']);
+                break;
+            default: 
+                echo "Invalid Action";
         }
     }
 ?>
---
+++ student.php
@@ -0,0 +1,125 @@
+<?php
+    require_once 'config.php';
+
+    class Student{
+        
+        private $db;
+
+        public function __construct(){
+            $database = new Database();
+            $this->db = $database->getConnection();
+        }
+
+        //Function for add a new students
+        public function addStudent($name, $birth, $gender, $resume){
+
+            $birthDate = new DateTime($birth);
+            $today = new DateTime();
+            $age = $today->diff($birthDate)->y;
+
+            $uploadDir = 'uploads/';
+            if(!is_dir($uploadDir)){
+                mkdir($uploadDir, 0777, true);
+            }
+
+            $fileName = uniqid() . '_' . basename($resume['name']);
+            $filePath = $uploadDir . $fileName;
+
+            if(move_uploaded_file($resume['tmp_name'], $filePath)){
+                $sql = "INSERT INTO student (nam, birth, age, gender, resume) VALUES (?, ?, ?, ?, ?)";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bind_param("ssiss", $name, $birth, $age, $gender, $filePath);
+
+                if($stmt->execute()){
+                    return "Student added Successfully";
+                }else{
+                    return "Error Adding Student" . $stmt->error;
+                }
+            }else{
+                return "Error Uploading resume";
+            }
+        }
+
+        //Function for Fetch all students
+        public function fetchStudents(){
+
+            $sql = "SELECT * FROM student";
+            $result = $this->db->query($sql);
+
+            if($result){
+                $student = $result->fetch_all(MYSQLI_ASSOC);
+                return json_encode($student);
+            }else{
+                return json_encode(["Error" => "Error Fetching Students"]);
+            }
+
+
+        }
+
+         //Function for fetch a single student by ID
+        public function fetchStudentById($id){
+            $sql = "SELECT * FROM students WHERE id = ?";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if($result->num_rows > 0){
+                return json_encode($result->fetch_assoc());
+            }else{
+                return json_encode(["error" => "Student not found"]);
+            }
+        }
+
+        //Function for update student
+        public function updateStudent($id, $name, $birth, $gender, $resume){
+
+            $birthDate = new Datetime($birth);
+            $today = new DateTime();
+            $age = $today->diff($birthDate)->y;
+
+            $filePath = null;
+            if($resume['error' === UPLOAD_ERR_OK]){
+                $uploadDir = 'uploads/';
+                if(!is_dir($uploadDir)){
+                    mkdir($uploadDir, 0777, true); //making the directory if not exist and give read and write permission
+                }
+
+                $fileName = uniqid() . '_' . basename($resume['name']);
+                $filePath = $uploadDir . $fileName;
+
+                if(!move_uploaded_file($resume['tmp_name'], $filePath)){
+                    return "Error Uploading Resume";
+                }
+            }
+            if($filePath){
+                $sql = "UPDATE student SET name = ?, birth = ?, age =?, gender = ?, resume = ? WHERE id = ?";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bind_param("ssissi", $name, $birth, $age, $gender, $filePath, $id);
+            }else{
+                $sql = "UPDATE student SET name = ?, birth = ?, age = ?, gender = ? WHERE id = ?";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bind_param("ssisi", $name, $birth, $age, $gender, $id);   
+            }
+            if ($stmt->execute()){
+                return "Student Updated Successfully";
+            }else{
+                return "Error updating student" . $stmt->error;
+            }
+
+        }
+
+        //Function for delete student
+        public function deleteStudent($id){
+            $sql = "DELETE FROM student WHERE id = ?";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bind_param("i", $id);
+
+            if($stmt->execute()){
+                return "Student deleted successfully";
+            }else{
+                return "Error deleting student : " . $stmt->error;
+            }
+        }
+    }
+?>
