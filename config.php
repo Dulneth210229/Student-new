d694a07813e5e49195fe0d7de858beb8c172873b
Dulneth210229 <dulneth.sa@gmail.com>
2025-02-10 10:44:42 +0530

    established the database connection

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+
+    class Database{
+        private $server = 'localhost';
+        private $user = 'root';
+        private $password = '';
+        private $name = 'student';
+        private $port = 3308;
+        private $conn;
+
+        public function __construct()
+        {
+            try{
+                $this->conn = new mysqli($this->server, $this->user, $this->password, $this->name, $this->port);
+                if($this->conn->connect_error){
+                    throw new Exception("Database connection failed: " . $this->conn->connect_error);
+                }
+
+
+            }catch(Exception $e){
+                die($e->getMessage());
+            }
+            
+        }
+        public function getConnection(){
+            return $this->conn;
+        }
+    
+
+    }
+
+?>
